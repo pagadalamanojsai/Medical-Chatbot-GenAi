3f8f5a0dde8796420c3cd8a5698f0b817f77d8d4
pagadalamanojsai <manojsaipagadala@gmail.com>
2025-05-28 07:23:04 -0400

    Add initial project structure and setup instructions in README; create necessary files and directories

---
+++ setup.py
@@ -0,0 +1,22 @@
+# setup.py
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="medical_chatbot_genai",
+#     version="0.1.0",
+#     packages=find_packages(),
+#     description="Medical Chatbot GenAI",
+# )
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Manoj Sai Pagadala',
+    author_email= 'manojsaipagadala@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
